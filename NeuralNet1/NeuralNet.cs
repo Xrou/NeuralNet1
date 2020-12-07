746bd7fe6ceb9a7940fc0d20bf11ca1cf7562860
xrou <royashen@mail.ru>
2020-12-07 22:06:42 +0700

    Edit

--- NeuralNet1/NeuralNet.cs
+++ NeuralNet1/NeuralNet.cs
@@ -10,10 +10,12 @@ namespace NeuralNet
     {
         private const float E = 2.7182818285f;
 
-        private int OutputLayersCount;
+        private int OutputsCount;
 
         // веса на вход нейрона
         private List<List<List<float>>> Weights = new List<List<List<float>>>(); // слой, номер нейрона в слое, номер связи
+        private List<List<List<float>>> WeightsDeltas = new List<List<List<float>>>(); // слой, номер нейрона в слое, номер связи
+
         private List<List<float>> Outputs = new List<List<float>>(); // слой, номер нейрона в слое
 
         public Net(int[] layersData) // layersData - номер слоя, кол-во нейронов в слое
@@ -21,21 +23,26 @@ namespace NeuralNet
             for (int layer = 1; layer < layersData.Length; layer++) // перебираем слои, пропускаем входной слой
             {
                 Weights.Add(new List<List<float>>());
+                WeightsDeltas.Add(new List<List<float>>());
+
                 Outputs.Add(new List<float>());
 
                 for (int neuron = 0; neuron < layersData[layer]; neuron++)
                 {
                     Weights[layer - 1].Add(new List<float>()); // добавляем нейрон
+                    WeightsDeltas[layer - 1].Add(new List<float>()); // добавляем нейрон
+
                     Outputs[layer - 1].Add(0f);
 
                     for (int inputsNeuron = 0; inputsNeuron < layersData[layer - 1]; inputsNeuron++)
                     {
                         Weights[layer - 1][neuron].Add(0.5f); // добавляем веса нейрону
+                        WeightsDeltas[layer - 1][neuron].Add(0.5f); // добавляем дельты веса нейрону
                     }
                 }
             }
 
-            OutputLayersCount = layersData[layersData.Length - 1];
+            OutputsCount = layersData[layersData.Length - 1];
         }
 
         public void Train(int epochs, int iterCount, float[][] learnData, float[][] testData, float[][] learnAnswers, float[][] testAnswers) //берем кол-во эпох, итераций в эпохе, данные для обучения, ответы
@@ -48,10 +55,34 @@ namespace NeuralNet
                     {
                         float[] output = Run(learnData[learnDataCounter]);
 
+                        #region print
                         foreach (float val in output)
                         {
                             Console.Write(val + "\t");
                         }
+                        #endregion
+
+                        float Error = 0; //MSE error
+
+                        #region MSE calc
+                        for (int i = 0; i < OutputsCount; i++)
+                        {
+                            Error = output[i] - learnAnswers[learnDataCounter][i];
+                        }
+
+                        Error /= OutputsCount;
+                        #endregion
+
+                        float[] deltaOutput = new float[OutputsCount];
+
+                        #region Deltas outputs calc
+                        for (int i = 0; i < OutputsCount; i++)
+                        {
+                            deltaOutput[i] = (learnAnswers[learnDataCounter][i] - output[i]) * DerivedActivation(output[i]);
+                        }
+                        #endregion
+
+
                     }
 
                     if (iter % 10 == 0 && iter != 0)
@@ -103,6 +134,11 @@ namespace NeuralNet
         private float Activation(float x)
         {
             return (float)(1 / (1 + Math.Pow(E, -x)));
+        }
+
+        private float DerivedActivation(float x)
+        {
+            return (1 - x) * x;
         }
     }
 }
