ff1d25a5d0b4df9761d38c31670d1e7684a3972a
xrou <royashen@mail.ru>
2020-12-27 22:12:56 +0700

    edit

--- NeuralNetRun/Program.cs
+++ NeuralNetRun/Program.cs
@@ -15,10 +15,10 @@ namespace NeuralNetRun
     {
         static void Main(string[] args)
         {
-            float min = Utils.FindMin(Data.learnDataFnet, Data.learnDataJnet, Data.testDataFnet, Data.testDataJnet, Data.learnAnswersFnet, Data.learnAnswersJnet, Data.testAnswersFnet, Data.testAnswersJnet);
+            float min = Utils.FindMin(Data.learnDataFnet, Data.learnDataJnet, Data.testDataFnet, Data.testDataJnet, Data.learnAnswersFnet, Data.learnAnswersJnet, Data.testAnswersFnet, Data.testAnswersJnet); //ищем минимум и максимум из всех данных
             float max = Utils.FindMax(Data.learnDataFnet, Data.learnDataJnet, Data.testDataFnet, Data.testDataJnet, Data.learnAnswersFnet, Data.learnAnswersJnet, Data.testAnswersFnet, Data.testAnswersJnet);
 
-            Normalize.ApplyMinimax(ref Data.learnDataFnet, min, max);
+            Normalize.ApplyMinimax(ref Data.learnDataFnet, min, max); //приводим к диапозону [0, 1]
             Normalize.ApplyMinimax(ref Data.learnDataJnet, min, max);
             Normalize.ApplyMinimax(ref Data.testDataFnet, min, max);
             Normalize.ApplyMinimax(ref Data.testDataJnet, min, max);
@@ -27,27 +27,29 @@ namespace NeuralNetRun
             Normalize.ApplyMinimax(ref Data.testAnswersFnet, min, max);
             Normalize.ApplyMinimax(ref Data.testAnswersJnet, min, max);
 
-            FeedForwardNN FNet = new FeedForwardNN(new int[] { 3, 9, 9, 9, 1 }, Activation.Sigmoid, Activation.DerivedSigmoid);
-            FeedForwardNN JNet = new FeedForwardNN(new int[] { 4, 9, 9, 9, 1 }, Activation.Sigmoid, Activation.DerivedSigmoid);
+            FeedForwardNN FNet = new FeedForwardNN(new int[] { 3, 9, 9, 9, 9, 9, 1 }, Activation.Sigmoid, Activation.DerivedSigmoid); // инициализация сети
+            FeedForwardNN JNet = new FeedForwardNN(new int[] { 4, 9, 9, 9, 9, 9, 1 }, Activation.Sigmoid, Activation.DerivedSigmoid);
 
-            Task FNetThread = new Task(() => FNet.Train(5, 1000, 0.1f, 0.3f, Data.learnDataFnet, Data.testDataFnet, Data.learnAnswersFnet, Data.testAnswersFnet, logFileName: "FNet train log.txt"));
-            Task JNetThread = new Task(() => JNet.Train(5, 1000, 0.1f, 0.3f, Data.learnDataJnet, Data.testDataJnet, Data.learnAnswersJnet, Data.testAnswersJnet, logFileName: "JNet train log.txt"));
+            Task FNetTask = new Task(() => FNet.Train(5, 1000, 0.01f, 0.2f, Data.learnDataFnet, Data.testDataFnet, Data.learnAnswersFnet, Data.testAnswersFnet, logFileName: "FNet train log.txt")); // создаем таск по обучению
+            Task JNetTask = new Task(() => JNet.Train(5, 1000, 0.01f, 0.2f, Data.learnDataJnet, Data.testDataJnet, Data.learnAnswersJnet, Data.testAnswersJnet, logFileName: "JNet train log.txt"));
 
-            FNetThread.Start();
-            JNetThread.Start();
+            FNetTask.Start(); // запускаем обучение в два потока
+            JNetTask.Start();
 
-            FNetThread.Wait();
-            JNetThread.Wait();
+            FNetTask.Wait(); // ждем пока сети обучатся
+            JNetTask.Wait();
 
-            float na11 = Normalize.Minimax(33, min, max);//0.12
-            float na12 = Normalize.Minimax(48, min, max);//0.35
-            float na21 = Normalize.Minimax(35, min, max);//0.15
-            float na22 = Normalize.Minimax(43, min, max);//0.27
+            // смотрим пример из тестов
 
-            float[] a11 = FNet.Run(new float[] { 35, 43, 26 }); //33
-            float[] a12 = FNet.Run(new float[] { 46, 54, 44 }); //48
-            float[] a21 = JNet.Run(new float[] { 35, 35, 38, 26 }); //35
-            float[] a22 = JNet.Run(new float[] { 46, 49, 50, 44 }); //43
+            float nF1 = Normalize.Minimax(33, min, max);
+            float nF2 = Normalize.Minimax(48, min, max);
+            float nJ1 = Normalize.Minimax(35, min, max);
+            float nJ2 = Normalize.Minimax(43, min, max);
+
+            float aF1 = FNet.Run(new float[] { 35, 43, 26 })[0];
+            float aF2 = FNet.Run(new float[] { 46, 54, 44 })[0];
+            float aJ1 = JNet.Run(new float[] { 35, 35, 38, 26 })[0];
+            float aJ2 = JNet.Run(new float[] { 46, 49, 50, 44 })[0];
         }
     }
 }
